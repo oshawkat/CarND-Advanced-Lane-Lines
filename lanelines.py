c51720551950764904e250c010b4d017b7f441ea
Osman Shawkat <osman.shawkat@havalus.com>
2019-01-29 20:59:44 -0500

    Add missing doc-strings and fix PEP coding style errors

--- lanelines.py
+++ lanelines.py
@@ -1,10 +1,12 @@
 import numpy as np
 import cv2
-import glob
-import matplotlib.pyplot as plt
+
 
 def calibrate_camera(fnames, grid_x, grid_y):
     """Calibrate camera using checkerboard images
+
+    This method leverages code from Udacity's Self-Driving Car Nanodegree
+    lectures
 
         Input:
             fnames: file names for the checkerboard images
@@ -23,22 +25,22 @@ def calibrate_camera(fnames, grid_x, grid_y):
 
     # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
     objp = np.zeros((grid_x * grid_y, 3), np.float32)
-    objp[:,:2] = np.mgrid[0:grid_x,0:grid_y].T.reshape(-1,2)
+    objp[:, :2] = np.mgrid[0:grid_x, 0:grid_y].T.reshape(-1, 2)
 
     # Arrays to store object points and image points from all the images.
-    objpoints = [] # 3d points in real world space
-    imgpoints = [] # 2d points in image plane.
+    objpoints = []  # 3d points in real world space
+    imgpoints = []  # 2d points in image plane.
 
     # Step through the list and search for chessboard corners
     for fname in fnames:
         img = cv2.imread(fname)
-        gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
         # Find the chessboard corners
-        ret, corners = cv2.findChessboardCorners(gray, (grid_x, grid_y),None)
+        ret, corners = cv2.findChessboardCorners(gray, (grid_x, grid_y), None)
 
         # If found, add object points, image points
-        if ret == True:
+        if ret is True:
             objpoints.append(objp)
             imgpoints.append(corners)
 
@@ -49,24 +51,25 @@ def calibrate_camera(fnames, grid_x, grid_y):
 
     return mtx, dist, rvecs, tvecs
 
+
 def rescale_img(img, max_val=255, min_val=0):
     """Rescale image to within the specified bounds
-    
+
     Input:
         img: image or numpy array to be scaled
         max_val: greatest element value in output
         min_val: smallest element value in output
-    
+
     Output:
         Numpy array of type np.uint8 with values ranging
         from as little as min_val up to max_val, inclusive
     """
-    
-    # output = np.uint8((img - np.min(img)) / np.max(img) * (max_val - min_val) + min_val)
+
     output = np.uint8(img / np.max(img) * max_val)
     output = np.maximum(output, np.full_like(output, min_val))
-    
+
     return output
+
 
 def gradient_threshold(img, kernel_size, gradmag_min, gradmag_max, dog_min,
                        dog_max):
@@ -91,7 +94,7 @@ def gradient_threshold(img, kernel_size, gradmag_min, gradmag_max, dog_min,
     # Calculate gradients in x and y directions
     sobel_x = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=kernel_size)
     sobel_y = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=kernel_size)
-    
+
     # Calculate the magnitude of the gradient and apply threshold
     sobel_mag = np.sqrt(np.power(sobel_x, 2) + np.power(sobel_y, 2))
     gradmag_bin = binary_threshold(sobel_mag, gradmag_min, gradmag_max)
@@ -101,109 +104,63 @@ def gradient_threshold(img, kernel_size, gradmag_min, gradmag_max, dog_min,
     sobel_y = np.abs(sobel_y)
     dog = np.arctan2(sobel_y, sobel_x)
     dog_bin = binary_threshold(dog, dog_min, dog_max)
-    
+
     return gradmag_bin, dog_bin
 
-def gradient_mag_threshold(img, kernel_size=3, min_val=0, max_val=255):
-    """Apply gradient magnitude thresholding to image
-    
-    Input:
-        img: greyscale image or numpy array
-        kernel_size: size of sobel kernel to use.  Must be odd
-        min_val: minimum allowed gradient magnitude
-        max_val: maximum allowed gradient magnitude
-    
-    Output:
-        Binary numpy array with inliers set to 1 and
-        all other elements to 0    
-    """
-    
-    # Calculate gradients in x and y directions
-    sobel_x = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=kernel_size)
-    sobel_y = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=kernel_size)
-    
-    # Calculate the magnitude of the gradient
-    sobel_mag = np.sqrt(np.power(sobel_x, 2) + np.power(sobel_y, 2))
-        
-    # Rescale Sobel magnitude
-    sobel_norm = rescale_img(sobel_mag)
-    
-    # Create a binary mask from thresholds
-    sobel_mask = np.zeros_like(sobel_norm)
-    sobel_mask[(sobel_norm >= min_val) & (sobel_norm <= max_val)] = 1
-    
-    return sobel_mask
-
-def gradient_dog_threshold(img, kernel_size=3, min_val=0, max_val=255):
-    """Apply Direction of Gradient (DOG) thresholding to image
-    
-    Input:
-        img: greyscale image or numpy array
-        kernel_size: size of sobel kernel to use.  Must be odd
-        min_val: minimum allowed DOG
-        max_val: maximum allowed DOG
-    
-    Output:
-        Binary numpy array with inliers set to 1 and
-        all other elements to 0
-    """
-    
-    # Calculate gradients in x and y directions
-    sobel_x = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=kernel_size)
-    sobel_y = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=kernel_size)
-    
-    # Take the absolute value of the x and y gradients
-    sobel_x = np.abs(sobel_x)
-    sobel_y = np.abs(sobel_y)
-    
-    # Calculate the direction of the gradient 
-    gradient = np.arctan2(sobel_y, sobel_x)
-    
-    # Create a binary mask of DOG from thresholds
-    sobel_mask = np.zeros_like(gradient)
-    sobel_mask[(gradient >= min_val) & (gradient <= max_val)] = 1
-    
-    return sobel_mask
 
 def binary_threshold(array, min_val, max_val):
     """Return a binary array of inliers
-    
+
     Input:
         array: input array to be thresholded against
         min_val: lowest inlier value, inclusive
         max_val: greatest inlier value, inclusive
-    
+
     Output:
         Returns numpy array of the same size as input
-        array.  Inliers set to 1, outliers to 0    
+        array.  Inliers set to 1, outliers to 0
     """
-    
+
     output = np.zeros_like(array)
     output[(array >= min_val) & (array <= max_val)] = 1
-    
+
     return output
 
+
 def find_lane_pixels(binary_warped):
+    """Find lane pixels using a sliding-window approach
+
+    This method leverages code from Udacity's Self-Driving Car Nanodegree
+    lectures
+
+        Input:
+            binary_warped: binary scene image from top-down view
+
+        Output:
+            leftx: x component of pixels in the left lane
+            lefty: corresponding y component of pixels in the left lane
+            rightx: x component of pixels in the right lane
+            righty: corresponding y component of pixels in the right lane
+            out_img: input image overlaid with the sliding window boxes found
+    """
+
     # Take a histogram of the bottom half of the image
-    histogram = np.sum(binary_warped[binary_warped.shape[0]//2:,:], axis=0)
+    histogram = np.sum(binary_warped[binary_warped.shape[0] // 2:, :], axis=0)
     # Create an output image to draw on and visualize the result
     out_img = np.dstack((binary_warped, binary_warped, binary_warped))
     # Find the peak of the left and right halves of the histogram
     # These will be the starting point for the left and right lines
-    midpoint = np.int(histogram.shape[0]//2)
+    midpoint = np.int(histogram.shape[0] // 2)
     leftx_base = np.argmax(histogram[:midpoint])
     rightx_base = np.argmax(histogram[midpoint:]) + midpoint
 
     # HYPERPARAMETERS
-    # Choose the number of sliding windows
-    nwindows = 9
-    # Set the width of the windows +/- margin
-    margin = 100
-    # Set minimum number of pixels found to recenter window
-    minpix = 50
+    nwindows = 9    # Choose the number of sliding windows
+    margin = 100    # Set the width of the windows +/- margin
+    minpix = 50     # Set minimum number of pixels found to recenter window
 
     # Set height of windows - based on nwindows above and image shape
-    window_height = np.int(binary_warped.shape[0]//nwindows)
+    window_height = np.int(binary_warped.shape[0] // nwindows)
     # Identify the x and y positions of all nonzero pixels in the image
     nonzero = binary_warped.nonzero()
     nonzeroy = np.array(nonzero[0])
@@ -219,79 +176,105 @@ def find_lane_pixels(binary_warped):
     # Step through the windows one by one
     for window in range(nwindows):
         # Identify window boundaries in x and y (and right and left)
-        win_y_low = binary_warped.shape[0] - (window+1)*window_height
-        win_y_high = binary_warped.shape[0] - window*window_height
-        ### TO-DO: Find the four below boundaries of the window ###
+        win_y_low = binary_warped.shape[0] - (window + 1) * window_height
+        win_y_high = binary_warped.shape[0] - window * window_height
+
+        # Find the four below boundaries of the window
         win_xleft_low = max(leftx_current - margin, 0)
-        win_xleft_high = min(leftx_current + margin, binary_warped.shape[1] - 1)
+        win_xleft_high = min(leftx_current + margin,
+                             binary_warped.shape[1] - 1)
         win_xright_low = max(rightx_current - margin, 0)
-        win_xright_high = min(rightx_current + margin, binary_warped.shape[1] - 1)
-        
+        win_xright_high = min(rightx_current + margin,
+                              binary_warped.shape[1] - 1)
+
         # Draw the windows on the visualization image
-        cv2.rectangle(out_img,(win_xleft_low,win_y_low),
-        (win_xleft_high,win_y_high),(0,255,0), 2) 
-        cv2.rectangle(out_img,(win_xright_low,win_y_low),
-        (win_xright_high,win_y_high),(0,255,0), 2) 
-        
-        ### TO-DO: Identify the nonzero pixels in x and y within the window ###
-        good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & 
-        (nonzerox >= win_xleft_low) &  (nonzerox < win_xleft_high)).nonzero()[0]
-        good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & 
-        (nonzerox >= win_xright_low) &  (nonzerox < win_xright_high)).nonzero()[0]
-        
+        cv2.rectangle(out_img, (win_xleft_low, win_y_low),
+                      (win_xleft_high, win_y_high), (0, 255, 0), 2)
+        cv2.rectangle(out_img, (win_xright_low, win_y_low),
+                      (win_xright_high, win_y_high), (0, 255, 0), 2)
+
+        # Identify the nonzero pixels in x and y within the window
+        good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) &
+                          (nonzerox >= win_xleft_low) &
+                          (nonzerox < win_xleft_high)).nonzero()[0]
+        good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) &
+                           (nonzerox >= win_xright_low) &
+                           (nonzerox < win_xright_high)).nonzero()[0]
+
         # Append these indices to the lists
         left_lane_inds.append(good_left_inds)
         right_lane_inds.append(good_right_inds)
-        
-        ### TO-DO: If you found > minpix pixels, recenter next window ###
-        ### (`right` or `leftx_current`) on their mean position ###
 
+        # If more than the minpix number of pixels are found,
+        # recenter the next window
         if len(good_left_inds) > minpix:
             leftx_current = (int)(np.mean(nonzerox[good_left_inds]))
         if len(good_right_inds) > minpix:
             rightx_current = (int)(np.mean(nonzerox[good_right_inds]))
 
-    # Concatenate the arrays of indices (previously was a list of lists of pixels)
-    try:
-        left_lane_inds = np.concatenate(left_lane_inds)
-        right_lane_inds = np.concatenate(right_lane_inds)
-    except ValueError:
-        # Avoids an error if the above is not implemented fully
-        pass
+    # Concatenate the arrays of indices (was a list of lists of pixels)
+    left_lane_inds = np.concatenate(left_lane_inds)
+    right_lane_inds = np.concatenate(right_lane_inds)
 
     # Extract left and right line pixel positions
     leftx = nonzerox[left_lane_inds]
-    lefty = nonzeroy[left_lane_inds] 
+    lefty = nonzeroy[left_lane_inds]
     rightx = nonzerox[right_lane_inds]
     righty = nonzeroy[right_lane_inds]
 
     return leftx, lefty, rightx, righty, out_img
 
+
 def fit_polynomial(binary_warped):
+    """Determine best-fit polynomials for two lane lines
+
+    Produces 2nd order polynomials that best fit detected lane lines from an
+    input image.  This method leverages code from Udacity's Self-Driving Car
+    Nanodegree lectures
+
+    Lane line fit is given as [A, B, C] where x = Ay^2 + By + C
+
+        Input:
+            binary_warped: binary scene image from top-down view
+
+        Output:
+            left_fit: left lane line best-fit polynomial coefficients
+            right_fit: left lane line best-fit polynomial coefficients
+            left_fitx: x coordinates of left best-fit line for plotting
+            right_fitx: x coordinates of right best-fit line for plotting
+            ploty: y coordinates corresponding to left_fitx and right_fitx for
+                plotting
+            out_img: input image overlaid with sliding window search boxes and
+                best-fit lines
+    """
+
     # Find our lane pixels first
     leftx, lefty, rightx, righty, out_img = find_lane_pixels(binary_warped)
 
-    ### TO-DO: Fit a second order polynomial to each using `np.polyfit` ###
+    # Fit a second order polynomial to each using `np.polyfit`
     left_fit = np.polyfit(lefty, leftx, 2)
     right_fit = np.polyfit(righty, rightx, 2)
 
     # Generate x and y values for plotting
-    ploty = np.linspace(0, binary_warped.shape[0]-1, binary_warped.shape[0] )
+    ploty = np.linspace(0, binary_warped.shape[0] - 1, binary_warped.shape[0])
     try:
-        left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
-        right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
+        left_fitx = (left_fit[0] * ploty ** 2 + left_fit[1] * ploty +
+                     left_fit[2])
+        right_fitx = (right_fit[0] * ploty ** 2 + right_fit[1] * ploty +
+                      right_fit[2])
     except TypeError:
-        # Avoids an error if `left` and `right_fit` are still none or incorrect
+        # Avoids an error if `left` and `right_fit` are none or incorrect
         print('The function failed to fit a line!')
-        left_fitx = 1*ploty**2 + 1*ploty
-        right_fitx = 1*ploty**2 + 1*ploty
+        left_fitx = 1 * ploty ** 2 + 1 * ploty
+        right_fitx = 1 * ploty ** 2 + 1 * ploty
 
-    ## Visualization ##
+    # Visualization
     # Colors in the left and right lane regions
     out_img[lefty, leftx] = [255, 0, 0]
     out_img[righty, rightx] = [0, 0, 255]
 
     return left_fit, right_fit, left_fitx, right_fitx, ploty, out_img
+
 
 def draw_lane_line_area(left_fitx, right_fitx, ploty, img_shape):
     """Draw valid lane region on a blank canvas
@@ -315,28 +298,29 @@ def draw_lane_line_area(left_fitx, right_fitx, ploty, img_shape):
     # Recast the x and y points into usable format for cv2.fillPoly()
     pts_left = np.array([np.transpose(np.vstack([left_fitx, ploty]))])
     pts_right = np.array([np.flipud(np.transpose(
-                    np.vstack([right_fitx, ploty])))])
+        np.vstack([right_fitx, ploty])))])
     pts = np.hstack((pts_left, pts_right))
 
     # Draw the valid lane region onto the warped blank image
-    cv2.fillPoly(valid_lane_region, np.int_([pts]), (0,255, 0))
+    cv2.fillPoly(valid_lane_region, np.int_([pts]), (0, 255, 0))
 
     return valid_lane_region
 
 def solve_poly_at(poly, eval_pt=0):
     """Solve a 2nd deg polynomial function for a given input
-    
+
     y = f(x) = Ax^2 + Bx + C
-    
+
     Input:
         poly: 2nd degree polynomial coefficients in the form [A, B, C]
         eval_pt: x value polynomial should be evaluated at
-    
+
     Output:
-        Value of function evaluated at eval_pt    
+        Value of function evaluated at eval_pt
     """
-    
+
     return poly[0]*eval_pt**2 + poly[1]*eval_pt + poly[2]
+
 
 def measure_curvature_pixels(poly_fit, y_eval=0):
     '''Calculate curvature of 2nd order polynomial
